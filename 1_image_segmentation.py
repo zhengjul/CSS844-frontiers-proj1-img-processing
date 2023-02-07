e89f3139f3ca46e5823d9cbef51de7a25719f109
Julia Zheng <zhengjul@msu.edu>
2023-02-07 13:33:46 -0500

    Add files via upload

---
+++ 1_image_segmentation.py
@@ -0,0 +1,105 @@
+import glob
+import math 
+import numpy as np
+import cv2
+from imageio import imread, imsave
+import os
+import os.path
+
+# https://stackoverflow.com/questions/72062001/remove-everything-of-a-specific-color-with-a-color-variation-tolerance-from-an
+
+def colour_thresh_segmentation(infile, outfile):
+ #NOTE we switched from RGB to BGR by using cv2, thus the colours below are BGR
+ image = cv2.imread(infile)
+
+ ### removing background ###
+ #remove green background
+ u_green = np.array([60, 75, 50],np.uint8)
+ l_green = np.array([20, 40, 20],np.uint8)
+ mask1 = cv2.inRange(image, l_green, u_green)
+
+ old_high = np.array([10,20,20],np.uint8)
+ old_low = np.array([0, 10, 0],np.uint8)
+ mask2 = cv2.inRange(image, old_low, old_high)
+
+ old_high = np.array([63,95,68],np.uint8)
+ old_low = np.array([48, 75, 45],np.uint8)
+ mask3 = cv2.inRange(image, old_low, old_high)
+
+ old_high = np.array([75,120,75],np.uint8)
+ old_low = np.array([68, 110, 65],np.uint8)
+ mask = cv2.inRange(image, old_low, old_high)
+ kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (400,400))
+ mask4 = cv2.dilate(mask, kernel, iterations=1)
+
+ old_high = np.array([25,40,25],np.uint8)
+ old_low = np.array([16, 32, 14],np.uint8)
+ mask = cv2.inRange(image, old_low, old_high)
+ kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
+ mask5 = cv2.dilate(mask, kernel, iterations=1)
+
+ #combine all masks to fully remove background
+ combined_mask = mask1 | mask2 | mask3 |mask4 | mask5
+ image[combined_mask>0] = [0,0,0]
+
+ ### remove yellow tag only in the top left corner 25% of image ###
+ h,w,channel = image.shape
+ subsect = image[0:math.floor(h/2),0:math.floor(w/2),:]
+
+ old_high = np.array([65, 180, 200],np.uint8)
+ old_low = np.array([60, 170, 190],np.uint8)
+ mask = cv2.inRange(subsect, old_low, old_high)
+ kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (100,100))
+ mask1 = cv2.dilate(mask, kernel, iterations=1)
+
+ old_high = np.array([70, 190, 215],np.uint8)
+ old_low = np.array([60, 180, 205],np.uint8)
+ mask = cv2.inRange(subsect, old_low, old_high)
+ kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (100,100)) # remove within 100pixel region
+ mask2 = cv2.dilate(mask, kernel, iterations=1)
+
+ old_high = np.array([50, 140, 165],np.uint8)
+ old_low = np.array([45, 130, 155],np.uint8)
+ mask = cv2.inRange(subsect, old_low, old_high)
+ kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (100,100)) # remove within 100pixel region
+ mask3 = cv2.dilate(mask, kernel, iterations=1)
+
+ old_high = np.array([85, 210, 240],np.uint8)
+ old_low = np.array([70, 195, 220],np.uint8)
+ mask = cv2.inRange(subsect, old_low, old_high)
+ kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (100,100)) # remove within 100pixel region
+ mask4 = cv2.dilate(mask, kernel, iterations=1)
+
+ # grey text on yellow tag
+ old_high = np.array([65, 65, 55],np.uint8)
+ old_low = np.array([60, 60, 50],np.uint8)
+ mask = cv2.inRange(subsect, old_low, old_high)
+ kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10,10)) # remove within 100pixel region
+ mask5 = cv2.dilate(mask, kernel, iterations=1)
+
+ # white part of yellow tag
+ old_high = np.array([250, 250, 250],np.uint8)
+ old_low = np.array([210, 210, 210],np.uint8)
+ mask = cv2.inRange(subsect, old_low, old_high)
+ kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (100,100)) # remove within 100pixel region
+ mask6 = cv2.dilate(mask, kernel, iterations=1)
+
+ #combine all masks to clean out the yellow tag marker in the top left section of the image (25% of image)
+ combined_mask = mask1 | mask2 | mask3 | mask4 | mask5 | mask6
+ subsect[combined_mask>0] = [0,0,0]
+ image[0:math.floor(h/2),0:math.floor(w/2),:] = subsect
+
+ #write out segmented image
+ cv2.imwrite(outfile, image)
+ return
+
+if __name__ == "__main__":
+
+ indir = '/mnt/home/zhengjul/class/frontiers/images/'
+ outdir='/mnt/home/zhengjul/class/frontiers/work/1_segmentation/'
+ for infile in glob.glob(indir+"*"):
+  outfile = outdir + os.path.basename(infile)
+  if not os.path.isfile(outfile):
+    print(outfile) 
+    colour_thresh_segmentation(infile, outfile)
+
---
+++ 2_plantcv.py
@@ -0,0 +1,179 @@
+from imageio import imread, imsave
+import numpy as np
+from plantcv import plantcv as pcv
+import cv2
+import os
+import glob
+import pandas as pd
+import os.path
+
+# source https://github.com/nrt-impacts/CSS844-module3-Hollender/blob/main/2021-4-16_2D_Branch_Angle_Estimation_JK.ipynb
+
+def indexer(array):
+    to_process = np.where(array == 255)
+    ys = to_process[0]
+    xs = to_process[1]
+    coords = []
+    for i in range(len(xs)):
+        coord = [xs[i],ys[i]]
+        coords.append(coord)
+    return coords
+
+## find base
+
+def find_base(tips):
+    y_coordinates = []
+    for i in range(len(tips)):
+        y_coordinates.append(tips[i][1])
+    max_index = np.argmax(y_coordinates)
+    to_return = tips[max_index]
+    return to_return
+
+## find branchpoint with smallest euclidian distance to base
+import math
+def find_branch_point(array,base):
+    to_process = np.where(array == 255)
+    ys = to_process[0]
+    xs = to_process[1]
+    coords = []
+    for i in range(len(xs)):
+        coord = [xs[i],ys[i]]
+        coords.append(coord)
+    coords_to_use = []
+    for i in range(len(coords)):
+        if coords[i] != base:
+            coords_to_use.append(coords[i])
+    distances = []
+    for i in range(len(coords_to_use)):
+        distance = math.sqrt((coords_to_use[i][0] - base[0])**2 + (coords_to_use[i][1] - base[1])**2)
+        distances.append(distance)
+    branch_point_index = np.argmin(distances)
+    branch_point_coord = coords_to_use[branch_point_index]        
+    return branch_point_coord
+
+# I've got the relevant branch point + the relevant base. Now, find the two tips farthest from the branch point. 
+
+def find_branch_tips(array,branch_point,base):
+    to_process = np.where(array == 255)
+    ys = to_process[0]
+    xs = to_process[1]
+    coords = []
+    for i in range(len(xs)):
+        coord = [xs[i],ys[i]]
+        coords.append(coord)
+    coords_to_use = []
+    for i in range(len(coords)):
+        if coords[i] != base:
+            coords_to_use.append(coords[i])
+    
+    # calculate distances of remaining tips to the branch point
+    distances = []
+    for i in range(len(coords_to_use)):
+        distance = math.sqrt((coords_to_use[i][0] - branch_point[0])**2 + (coords_to_use[i][1] - branch_point[1])**2)
+        distances.append(distance)
+    original_distances = distances
+    distances.sort()
+    first = distances[0]
+    second = distances[1]
+    top_index = original_distances.index(first)
+    second_index = original_distances.index(second)
+    
+    branch_tips = [coords_to_use[top_index],coords_to_use[second_index]]
+    return branch_tips
+
+# now we've got the base, our branch point, and our two things to compare against. Now
+# (1) branchpoint to branch tip 1, and branchpoint to branch tip 2 vectors
+# (2) Figure out the angle between them
+
+def branch_angle(branch_tips,branch_point):
+    first_branch_tip = branch_tips[0]
+    second_branch_tip = branch_tips[1]
+    branch_vector_one = [first_branch_tip[0] - branch_point[0],first_branch_tip[1] - branch_point[1]]
+    branch_vector_two = [second_branch_tip[0] - branch_point[0],second_branch_tip[1] - branch_point[1]]
+    
+    unit_vector_1 = branch_vector_one / np.linalg.norm(branch_vector_one)
+    unit_vector_2 = branch_vector_two / np.linalg.norm(branch_vector_two)
+    dot_product = np.dot(unit_vector_1, unit_vector_2)
+    angle_in_radians = np.arccos(dot_product)
+    angle_in_degrees = np.degrees(angle_in_radians)
+    
+    return angle_in_degrees
+
+## Put everything together into one function
+
+def calculate_branch_angle(tip_pts_mask,branch_pts_mask):
+    indices = indexer(tip_pts_mask)
+    base = find_base(indices)
+    branch_point = find_branch_point(branch_pts_mask,base)
+    branch_tips = find_branch_tips(tip_pts_mask,branch_point,base)
+    degrees = branch_angle(branch_tips,branch_point)
+    return degrees
+
+if __name__ == "__main__":
+ indir = '/mnt/home/zhengjul/class/frontiers/work/1_segmentation/'  #read in segmented images
+ outdir_binary='/mnt/home/zhengjul/class/frontiers/work/2_binary_thresh/' #output binary threshold images
+ outdir_plantcv='/mnt/home/zhengjul/class/frontiers/work/3_plantcv_img/' #output plantcv image results
+ outfile_tables='/mnt/home/zhengjul/class/frontiers/work/4_tables/tables.csv' #output into one table the mask area and output "root angle" (suspicious, skeletonize is too detailed for this to be correct based on visual inspection)
+
+ #create pandas dataframe 
+ df = pd.DataFrame(columns=['img','area','angle'])
+ # analyze all figures
+
+ for infile in glob.glob(indir+"*"):
+  filename = os.path.basename(infile)
+  outfile_binary = outdir_binary + filename
+  outfile_plantcv = outdir_plantcv + filename
+  print(filename)
+
+  # binarize image into black or white
+  image, path, filename = pcv.readimage(infile, mode="gray")
+  binary_img = pcv.threshold.binary(gray_img=image, threshold=10, max_value=255, object_type='light')
+  pcv.print_image(binary_img, outfile_binary)
+  print("output binary img")
+
+  # fill in any holes so that skeletonize won't capture too much details
+  binary_img = pcv.median_blur(gray_img=binary_img, ksize=5) # blur
+  mask = pcv.fill(bin_img=binary_img, size=500) # fill in holes
+
+  # manually crop the mask to focus on the lateral root angle
+  cropped_mask = mask[2000:4000, 2000:4000]
+
+  #skeletonize
+  skeleton = pcv.morphology.skeletonize(mask=cropped_mask)
+  # Adjust line thickness with the global line thickness parameter
+  pcv.params.line_thickness = 200 
+  pcv.print_image(skeleton, outfile_plantcv)
+  print("output skeleton img")
+
+  # Prune the skeleton  
+  pruned, seg_img, edge_objects = pcv.morphology.prune(skel_img=skeleton, size=0, mask=cropped_mask)
+
+  # Identify branch points   
+  branch_pts_mask = pcv.morphology.find_branch_pts(skel_img=skeleton, mask=cropped_mask, label="default")
+
+  # Identify tip points   
+  tip_pts_mask = pcv.morphology.find_tips(skel_img=skeleton, mask=None, label="default")
+
+  """
+  # Sort segments into branch objects and stem objects  
+  branch_obj, stem_obj = pcv.morphology.segment_sort(skel_img=skeleton, 
+                                                     objects=edge_objects,
+                                                     mask=cropped_mask)
+  # Identify segments     
+  segmented_img, labeled_img = pcv.morphology.segment_id(skel_img=skeleton,
+                                                           objects=branch_obj,
+                                                           mask=cropped_mask)
+  """
+  print("prior to calc angle")
+  # add results to df as new row
+  ret,thresh=cv2.threshold(binary_img,10,255,cv2.THRESH_BINARY_INV)
+  area = cv2.countNonZero(thresh)    # mask area
+  angle = calculate_branch_angle(tip_pts_mask,branch_pts_mask) # root angle
+  print("after calc angle")
+
+  row = {'img':filename, 'area':area, 'angle':angle}
+  df = df.append(row, ignore_index=True)
+  print("save row data "+str(area)+" "+str(angle))
+
+ # use pandas to export table to csv
+ df.to_csv(outfile_tables, index = False)
